c5d564e38c4d1eb85ed50a235ee0e0b408c54852
daniel-krgr <daniel-krugerdeal@hotmail.com>
2024-10-29 14:30:27 -0300

    primeiras telas
    Desenvolvimento das primeiras telas (login e cadastro)

---
+++ app/src/main/java/com/example/trabalhointegrado2bim/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.trabalhointegrado2bim;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView tvCadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+            }
+        });
+
+    }
+
+    private void IniciarComponentes() {
+        tvCadastro = findViewById(R.id.tvCadastro);
+    }
+
+}
