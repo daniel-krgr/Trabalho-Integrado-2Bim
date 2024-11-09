886c220a597b8d36915706a9fcc7ac6c18c4cb07
Rick-49 <ricardolemoscapelari@outlook.com>
2024-11-08 22:22:32 -0300

    Criado ação para o botão cadastrar, e trocado o tipo do edite

--- app/src/main/java/com/example/trabalhointegrado2bim/LoginActivity.java
+++ app/src/main/java/com/example/trabalhointegrado2bim/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.trabalhointegrado2bim;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,9 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class LoginActivity extends AppCompatActivity {
 
     private TextView tvCadastro;
+    private EditText edEmail;
+    private EditText edSenha;
+    private Button btEntrar;
+    String[] mensagem = {"preencha todos os campos",
+            "Cadastro realizado com sucesso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,34 @@ public class LoginActivity extends AppCompatActivity {
 
         IniciarComponentes();
 
+        // click no botão efetuar log-in
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = edEmail.getText().toString();
+                String senha = edSenha.getText().toString();
+
+                // verifica se foi digitado valores nos campos
+
+                if (email.isEmpty() || senha.isEmpty()){
+
+                    Snackbar snackbar = Snackbar.make(view,mensagem[0],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }else{
+                    // faz verificação dos dados no servidor
+
+
+                }
+
+            }
+        });
+
+
+        // click no botão criar uma conta
         tvCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +79,9 @@ public class LoginActivity extends AppCompatActivity {
 
     private void IniciarComponentes() {
         tvCadastro = findViewById(R.id.tvCadastro);
+        edEmail = findViewById(R.id.edEmail);
+        edSenha = findViewById(R.id.edSenha);
+        btEntrar = findViewById(R.id.btEntrar);
     }
 
 }
